b8af622fd7b1589a33457ea7864e489fbf3f2b19
kdc13 <kishan.chandravadia@gmail.com>
2023-01-12 02:30:06 +0530

    new

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function Login() {
+  const navigate= useNavigate()
+  const [login,setLogin]=useState({
+    username:"",
+    password:""
+  })
+
+  function onChangeHandler(e){
+    const {name,value} = e.target
+    setLogin((prevState)=>({
+      ...prevState,
+      [name]:value
+    }))
+  }
+  console.log(login)
+  function loginHandler(e){
+    e.preventDefault()
+    fetch('http://localhost:8765/auth/login',{
+      method:'POST',
+      body:JSON.stringify({
+          username: login.username,
+          password: login.password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+  })
+      .then(res=>res.json())
+      .then(json=>navigate("/"))
+  }
   return (
-    <div>Login</div>
+    <div className='container'>
+    <div className='signup width-30'>
+        <h1>Login</h1>
+        <form className='signup-form' onSubmit={loginHandler}>
+        <div className='persional-info'>
+            <div className='input-container'>
+            <div className='form-inputs'>
+                <label>User name</label>
+                <input type="text" name="username" value={login.username} onChange={onChangeHandler} />
+            </div>
+            </div>
+
+             <div className='input-container'>
+            <div className='form-inputs'>
+                <label>Password</label>
+                <input type="text" name="password" value={login.password} onChange={onChangeHandler} />
+            </div>
+
+            </div>
+
+            <button className='btn-signup'>Login</button>
+            </div>
+        </form>
+    </div>
+</div>
   )
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,108 +1,149 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
-
-    const [userDetails,setUserDetails]=useState( {
-        email:'',
-        username:'',
-        password:'',
-        name:{
-            firstname:'',
-            lastname:''
-        },
-        address:{
-            city:'',
-            street:'',
-            number:null,
-            zipcode:'',
-            geolocation:{
-                lat:'',
-                long:''
-            }
-        },
-        phone:''
+    const navigate = useNavigate()
+    const [massage, setMassage] = useState("")
+    const [userDetails, setUserDetails] = useState({
+        email: '',
+        username: '',
+        password: '',
+        firstname: '',
+        lastname: '',
+        city: '',
+        street: '',
+        number: null,
+        zipcode: '',
+        lat: '',
+        long: '',
+        phone: '',
     })
+
+
+    function onChangeHandler(e) {
+        const { name, value } = e.target;
+
+        setUserDetails((prevState) => {
+            return {
+                ...prevState,
+                [name]: value
+            }
+        })
+    }
+    console.log(userDetails)
+    function signupFormHandler(e) {
+        e.preventDefault();
+        let obj = {
+            email: userDetails.email,
+            username: userDetails.username,
+            password: userDetails.password,
+            name: {
+                firstname: userDetails.firstname,
+                lastname: userDetails.lastname
+            },
+            address: {
+                city: userDetails.city,
+                street: userDetails.street,
+                number: userDetails.number,
+                zipcode: userDetails.zipcode,
+                geolocation: {
+                    lat: userDetails.lat,
+                    long: userDetails.long
+                }
+            },
+            phone: userDetails.phone
+        }
+        fetch('http://localhost:8765/users', {
+            method: "POST",
+            body: JSON.stringify(obj),
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then(res => res.json())
+            .then(json => navigate("/login"))
+        
+    }
     return (
         <div className='container'>
-            <div className='signup'>
+            <div className='signup width-80'>
+                <h4>{massage}</h4>
                 <h1>Signup</h1>
-                <form className='signup-form'>
-                <div className='persional-info'>
-                <h2>Personal Information</h2>
-                    <div className='input-container'>
-                    <div className='form-inputs'>
-                        <label>First name</label>
-                        <input type="text" />
-                    </div>
-                        <div className='form-inputs'>
-                        <label>Last name</label>
-                        <input type="text" />
-                    </div>
-                    </div>
-                    <div className='input-container'>
-                    <div className='form-inputs'>
-                        <label>Email</label>
-                        <input type="text" />
-                    </div>
-                    </div>
-                    <div className='input-container'>
-                    <div className='form-inputs'>
-                        <label>User name</label>
-                        <input type="text" />
-                    </div>
-                    </div>
+                <form className='signup-form' onSubmit={signupFormHandler}>
+                    <div className='persional-info'>
+                        <h2>Personal Information</h2>
+                        <div className='input-container'>
+                            <div className='form-inputs'>
+                                <label>First name</label>
+                                <input type="text" name="firstname" required placeholder='first name' value={userDetails.firstname} onChange={onChangeHandler} />
+                            </div>
+                            <div className='form-inputs'>
+                                <label>Last name</label>
+                                <input type="text" name="lastname" required placeholder='Last name' value={userDetails.lastname} onChange={onChangeHandler} />
+                            </div>
+                        </div>
+                        <div className='input-container'>
+                            <div className='form-inputs'>
+                                <label>Email</label>
+                                <input type="email" name="email" required placeholder='Enter Email' value={userDetails.email} onChange={onChangeHandler} />
+                            </div>
+                        </div>
+                        <div className='input-container'>
+                            <div className='form-inputs'>
+                                <label>User name</label>
+                                <input type="text" name="username" required placeholder='Enter Username' value={userDetails.username} onChange={onChangeHandler} />
+                            </div>
+                        </div>
 
-                     <div className='input-container'>
-                    <div className='form-inputs'>
-                        <label>Password</label>
-                        <input type="text" />
-                    </div>
-                        <div className='form-inputs'>
-                        <label>Confirm password</label>
-                        <input type="text" />
-                    </div>
-                    </div>
-                    <div className='input-container'>
-                    <div className='form-inputs'>
-                        <label>Contact number</label>
-                        <input type="text" />
-                    </div>
-                    </div>
+                        <div className='input-container'>
+                            <div className='form-inputs'>
+                                <label>Password</label>
+                                <input type="password" name="password" required placeholder='Enter password' value={userDetails.password} onChange={onChangeHandler} />
+                            </div>
+                            <div className='form-inputs'>
+                                <label>Confirm password</label>
+                                <input type="password" required placeholder='Enter confirm pasword' />
+                            </div>
+                        </div>
+                        <div className='input-container'>
+                            <div className='form-inputs'>
+                                <label>Contact number</label>
+                                <input type="text" name="phone" required placeholder='Enter contact number' value={userDetails.phone} onChange={onChangeHandler} />
+                            </div>
+                        </div>
                     </div>
                     <div className='address'>
-                    <h2>Adderss</h2>
-                    <div className='input-container'>
-                    <div className='form-inputs'>
-                        <label>House number</label>
-                        <input type="text" />
-                    </div>
-                        <div className='form-inputs'>
-                        <label>Street</label>
-                        <input type="text" />
-                    </div>
-                    </div>
-                    <div className='input-container'>
-                    <div className='form-inputs'>
-                        <label>City</label>
-                        <input type="text" />
-                    </div>
-                        <div className='form-inputs'>
-                        <label>Zipcode</label>
-                        <input type="text" />
-                    </div>
-                    </div>
-                   
-                    <div className='input-container'>
-                    <div className='form-inputs'>
-                        <label>Latitude</label>
-                        <input type="text" />
-                    </div>
-                        <div className='form-inputs'>
-                        <label>Longitude</label>
-                        <input type="text" />
-                    </div>
-                    </div>
-                    <button className='btn-signup'>Sign up</button>
+                        <h2>Adderss</h2>
+                        <div className='input-container'>
+                            <div className='form-inputs'>
+                                <label>House number</label>
+                                <input type="number" name="number" required placeholder='Enter house number' value={userDetails.number} onChange={onChangeHandler} />
+                            </div>
+                            <div className='form-inputs'>
+                                <label>Street</label>
+                                <input type="text" name="street" required placeholder='Enter street name' value={userDetails.street} onChange={onChangeHandler} />
+                            </div>
+                        </div>
+                        <div className='input-container'>
+                            <div className='form-inputs'>
+                                <label>City</label>
+                                <input type="text" name="city" required placeholder='Enter city' value={userDetails.city} onChange={onChangeHandler} />
+                            </div>
+                            <div className='form-inputs'>
+                                <label>Zipcode</label>
+                                <input type="text" name="zipcode" required placeholder='Enter zipcode' value={userDetails.zipcode} onChange={onChangeHandler} />
+                            </div>
+                        </div>
+
+                        <div className='input-container'>
+                            <div className='form-inputs'>
+                                <label>Latitude</label>
+                                <input type="text" name="lat" required placeholder='Enter longitude' value={userDetails.lat} onChange={onChangeHandler} />
+                            </div>
+                            <div className='form-inputs'>
+                                <label>Longitude</label>
+                                <input type="text" name="long" required placeholder='Enter latitude' value={userDetails.long} onChange={onChangeHandler} />
+                            </div>
+                        </div>
+                        <button className='btn-signup'>Sign up</button>
                     </div>
                 </form>
             </div>
